8a087603300eb901028aecf4759f7d3927e991a4
Victor Souza Lima <victorsouzalima2064@gmail.com>
2024-08-20 19:29:31 -0300

    Implementation of "Game" class

---
+++ Game.java
@@ -0,0 +1,41 @@
+import java.time.LocalDate;
+
+class Game{
+    public int id;
+    public String name;
+    public String developer;
+    public float price;
+    public LocalDate releaseDate;
+
+    public Game(){
+        this(-1, "","", 0F, LocalDate.now());
+    }
+
+    public Game(String n, String d, float p, LocalDate rD){
+        this(-1, n, d, p, rD);
+    }
+
+    public Game(int id, String n, String d, float p, LocalDate rD){
+        this.id = id;
+        this.name = n;
+        this.developer = d;
+        this.price = p;
+        this.releaseDate = rD;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String toString(){
+        return "\nID.........:" + this.id +
+               "\nName.......:" + this.name +
+               "\nDeveloper..:" + this.developer +
+               "\nPrice......: $" + this.price +
+               "\nReleaseDate:" + this.releaseDate;
+    }
+}
