c1cbda1091321a20fb5e85c4fb9bedd80523a998
Victor Souza Lima <victorsouzalima2064@gmail.com>
2024-09-06 16:43:18 -0300

    Main addaptation to generic File methods

--- Estudos/GenericFile/Main.java
+++ Estudos/GenericFile/Main.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 import java.time.LocalDate;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
 
-        DataBase gameFile;
+        DataBase<Game> gameFile;
 
         Game g1 = new Game("Farcry 5", "Ubisoft", 250.00F, LocalDate.of(2018, 10, 12));
         Game g2 = new Game("Elden Ring", "From Software", 300.00F, LocalDate.of(2021, 9, 5));
@@ -15,31 +15,42 @@ public class Main {
 
         try {
 
-            File f = new File(".\\GenericFile\\data\\game.db");
+            File f = new File(".//Estudos//GenericFile//data//game.db");
             f.delete();
 
-            gameFile = new DataBase("game.db");
+            gameFile = new DataBase<>("game.db", Game.class.getConstructor());
 
             gameFile.create(g1);
             gameFile.create(g2);
             gameFile.create(g3);
 
-            Game g = gameFile.read("Elden Ring");
+            Game g = gameFile.read(3);
 
-            if(g != null){
+            if (g != null) {
                 System.out.println(g.toString());
-            }else{
+            } else {
                 System.out.println("\nGame not found");
             }
 
+
+            g3.name = "The Witcher 3";
+
+            gameFile.update(g3);
+
+            System.out.println();
+
+            g = gameFile.read(3);
+
+
+            if (g != null) {
+                System.out.println(g.toString());
+            } else {
+                System.out.println("\nGame not found");
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        
-
-        
-        
     }
 }
